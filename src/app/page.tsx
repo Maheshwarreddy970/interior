ec837eb78bca8a43bac589c54b9751946ada0fd4
Maheshwarreddy970 <mr8382455@gmail.com>
2024-10-15 11:37:06 +0530

    adding text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,8 @@ import { useEffect, useState } from "react";
 import Preloader from "../components/preloader";
 import Textparelex from "@/components/ui/Textparelex";
 import BlurFade from "@/components/ui/blur-fade";
+import data from "@/../data/home.json"
+import { cn } from "@/lib/utils";
 
 export default function Home() {
   const [isLoading, setIsLoading] = useState(true);
@@ -30,43 +32,26 @@ export default function Home() {
       </AnimatePresence>
       <Herosection></Herosection>
       {/* <LogoProof></LogoProof> */}
-      <div  className=" flex flex-col py-20 items-center gap-6 mb-16">
+      <div className=" flex flex-col py-20 items-center gap-6 mb-16">
         <BlurFade
           y={50}
           className="text-3xl text-center lg:text-5xl font-semibold mb-5 tracking-wide"
         >
-          OUR MISSION & VALUES
+          {data.overview.title}
         </BlurFade>
-        <motion.div  className=" w-full grid mt-20 lg:grid-cols-3 max-w-7xl mx-auto">
-          <Textparelex>
-            <div  className="text-xl text-left lg:text-center max-w-sm lg:text-xl ">
-              MISSION
-            </div>
-            <div  className=" text-left lg:text-center lg:text-lg max-w-sm">
-              To produce and distribute the highest quality of luxury furniture
-              products that stand for Design. Quality, Innovation and Value.
-            </div>
-          </Textparelex>
-          <Textparelex>
-            <div  className="text-xl text-right lg:text-center mt-7 lg:mt-36  max-w-sm lg:text-xl ">
-              VISION
-            </div>
-            <div  className=" text-right lg:text-center lg:text-lg max-w-sm">
-              To become the leading manufacturing house for bespoke furniture in the
-              world, catering to build luxurious masterpieces that are desired and
-              appreciated by not only the most elite, but also the most learned and
-              demanding industry architects, designers and consultants.{" "}
-            </div>
-          </Textparelex>
-          <Textparelex>
-            <div  className="text-xl text-left lg:text-center max-w-sm lg:text-xl ">
-              VALUES
-            </div>
-            <div  className=" text-left lg:text-center lg:text-lg max-w-sm">
-              To produce and distribute the highest quality furniture products that
-              stand for design, quality, innovation and value.
-            </div>
-          </Textparelex>
+        <motion.div className=" w-full grid -mt-10 lg:grid-cols-3 max-w-7xl mx-auto">
+          {
+            data.overview.info.map((info, i) => (
+              <Textparelex key={i}>
+                <div className={cn("text-xl text-left lg:text-center max-w-sm lg:text-xl ",i==1 && "mt-20")}>
+                  {info.heading}
+                </div>
+                <div className=" text-left lg:text-center lg:text-lg max-w-sm">
+                  {info.text}
+                </div>
+              </Textparelex>
+            ))
+          }
         </motion.div>
       </div>
       <Bento></Bento>
